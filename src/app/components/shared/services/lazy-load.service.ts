b9da09f9c720bcb35b93e1a8a932d0bbe92802fc
mehmetalp <mehmetakifalp@gmail.com>
2019-11-04 22:41:12 +0200

    lazy loaded script injector service and facebook service

---
+++ src/app/components/shared/services/lazy-load.service.ts
@@ -0,0 +1,39 @@
+import { ReplaySubject, Observable } from "rxjs";
+
+export class LazyLoadService {
+  loadedLibraries: { [url: string]: ReplaySubject<void> } = {};
+
+  load(url: string, type: "script" | "style"): Observable<void> {
+    if (!url) return;
+
+    const key = url.slice(url.lastIndexOf("/") + 1);
+
+    if (this.loadedLibraries[key]) {
+      return this.loadedLibraries[key].asObservable();
+    }
+
+    this.loadedLibraries[key] = new ReplaySubject();
+
+    const library = document.createElement(
+      type === "script" ? "script" : "link"
+    );
+
+    if (type === "script") {
+      library.type = "text/javascript";
+      (library as HTMLScriptElement).src = url;
+    } else {
+      library.type = "text/css";
+      (library as HTMLLinkElement).rel = "stylesheet";
+      (library as HTMLLinkElement).href = url;
+    }
+
+    library.onload = () => {
+      this.loadedLibraries[key].next();
+      this.loadedLibraries[key].complete();
+    };
+
+    document.body.appendChild(library);
+
+    return this.loadedLibraries[key].asObservable();
+  }
+}
