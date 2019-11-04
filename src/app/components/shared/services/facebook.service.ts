cd8309449eb7d01972fc66dc271e04edde057b29
mehmetalp <mehmetakifalp@gmail.com>
2019-11-04 23:15:08 +0200

    facebook service and font awesome

--- src/app/components/shared/services/facebook.service.ts
+++ src/app/components/shared/services/facebook.service.ts
@@ -1,6 +1,40 @@
 import { Injectable } from "@angular/core";
-
+import { LazyLoadService } from "./lazy-load.service";
+import { Observable, Subject } from "rxjs";
+declare const FB;
 @Injectable({
   providedIn: "root"
 })
-export class FacebookService {}
+export class FacebookService {
+  constructor(private lazyLoadService: LazyLoadService) {
+    this.lazyLoadService
+      .load("//connect.facebook.net/en-US/sdk.js", "script")
+      .subscribe(() => {
+        FB.init({
+          appId: "",
+          cookie: true,
+          xfbml: true,
+          version: "v3.0"
+        });
+      });
+  }
+
+  signIn(): Observable<any> {
+    const result = new Subject();
+
+    FB.login(
+      (fbRes: any) => {
+        if (fbRes.authResponse) {
+          result.next(fbRes.authResponse);
+          result.complete();
+        } else {
+          result.next(false);
+          result.complete();
+        }
+      },
+      { scope: "email,public_profile" }
+    );
+
+    return result;
+  }
+}
