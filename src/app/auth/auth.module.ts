e6cc41890fb2b374c56d2b6e0674e14b8a9e4a47
mehmetalp <mehmetakifalp@gmail.com>
2019-11-04 21:29:23 +0200

    login route problem fixed

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { LoginComponent } from "./login/login.component";
 import { LogoutComponent } from "./logout/logout.component";
 import { AuthRoutingModule } from "./auth-routing.module";
+import { SharedModule } from "../components/shared/shared.module";
 
 @NgModule({
   declarations: [LoginComponent, LogoutComponent],
-  imports: [AuthRoutingModule]
+  imports: [AuthRoutingModule, SharedModule]
 })
 export class AuthModule {}
