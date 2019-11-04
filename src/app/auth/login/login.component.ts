11588cea158c2f9e1c4cff5af9200df1f99eeae3
mehmetalp <mehmetakifalp@gmail.com>
2019-11-04 23:33:52 +0200

    facebook service, lazy load service, font awesome, demo app

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from "@angular/core";
+import { FacebookService } from "src/app/components/shared/services/facebook.service";
 
+declare const FB: any;
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.sass"]
 })
 export class LoginComponent implements OnInit {
+  constructor(
+    private cdRef: ChangeDetectorRef,
+    private facebookService: FacebookService
+  ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  loginWithFacebook() {
+    this.facebookService.signIn().subscribe((res) => {
+      console.log(res);
+
+      FB.api(`/me?fields=name,link`, (fbUser: any) => {
+        console.log(fbUser.name);
+        this.cdRef.detectChanges();
+      });
+    });
   }
-
 }
