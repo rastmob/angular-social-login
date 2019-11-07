0db999624bac9e7ede365fe2ead520de99b02de2
mehmetalp <mehmetakifalp@gmail.com>
2019-11-07 22:32:02 +0200

    google login service added

---
+++ src/app/components/shared/services/google.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from "@angular/core";
+import { LazyLoadService } from "./lazy-load.service";
+import { Observable, Subject } from "rxjs";
+declare const GO: any;
+@Injectable({
+  providedIn: "root"
+})
+export class GoogleService {
+  public auth2: any;
+  constructor(private lazyLoadService: LazyLoadService) {
+    this.lazyLoadService
+      .load(`https://apis.google.com/js/platform.js`, "script")
+      .subscribe(() => {
+        GO.load("auth2", () => {
+          this.auth2 = GO.auth2.init({
+            fetch_basic_profile: false,
+            scope: "profile email",
+            client_id:
+              "269788945797-f29pgcd8lvsnf4ujnoa7940bldmd3uc2.apps.googleusercontent.com"
+          });
+        });
+      });
+  }
+
+  signIn(): Observable<any> {
+    const result = new Subject();
+    this.auth2.signIn({ scope: "profile email" }).then(
+      () => {
+        const gToken = this.auth2.currentUser.get().getAuthResponse(true);
+        result.next(gToken);
+        result.complete();
+      },
+      function(fail) {
+        result.next(false);
+        result.complete();
+      }
+    );
+    return result;
+  }
+}
